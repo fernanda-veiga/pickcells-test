a7c5ba8f8a88e324133e07c3bb0db10c7b66ac4c
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-07-10 16:17:04 -0300

    Added a test to evaluate if the course with most obligatory credits was being returned.

--- pegarCursoComMaisCreditosObrigatorios.test.js
+++ pegarCursoComMaisCreditosObrigatorios.test.js
@@ -42,12 +42,47 @@ test("Pegar curso com mais créditos", () => {
   });
 });
 
-/*test("Pegar curso com mais créditos obrigatórios", () =>
-  expect(criarDisciplina("Algoritmos e Programação", 6, true)).toEqual({
-    nome: "Algoritmos e Programação",
-    creditos: 6,
-    obrigatoria: true,
-  }));*/
+test("Pegar curso com mais créditos obrigatórios", () => {
+  const cursos = [
+    {
+      nome: "Curso01",
+      tipo: "Graduação",
+      disciplinas: [
+        { nome: "Disciplina01", creditos: 5, obrigatoria: false },
+        { nome: "Disciplina02", creditos: 3, obrigatoria: false },
+        { nome: "Disciplina03", creditos: 4, obrigatoria: true },
+      ],
+    },
+    {
+      nome: "Curso02",
+      tipo: "Mestrado",
+      disciplinas: [
+        { nome: "Disciplina01", creditos: 6, obrigatoria: false },
+        { nome: "Disciplina02", creditos: 4, obrigatoria: false },
+        { nome: "Disciplina03", creditos: 5, obrigatoria: true },
+      ],
+    },
+    {
+      nome: "Curso03",
+      tipo: "Doutorado",
+      disciplinas: [
+        { nome: "Disciplina01", creditos: 4, obrigatoria: true },
+        { nome: "Disciplina02", creditos: 2, obrigatoria: false },
+        { nome: "Disciplina03", creditos: 3, obrigatoria: true },
+      ],
+    },
+  ];
+
+  expect(pegarCursoComMaisCreditosObrigatorios(cursos)).toEqual({
+    nome: "Curso03",
+    tipo: "Doutorado",
+    disciplinas: [
+      { nome: "Disciplina01", creditos: 4, obrigatoria: true },
+      { nome: "Disciplina02", creditos: 2, obrigatoria: false },
+      { nome: "Disciplina03", creditos: 3, obrigatoria: true },
+    ],
+  });
+});
 
 /*test("Pegar curso com mais créditos obrigatórios com desempate por tipo", () =>
   expect(criarDisciplina("Algoritmos e Programação", 6, true)).toEqual({
