897c4e9a5a9360f1430fc08335250a9590e7f951
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-07-10 17:40:48 -0300

    Added verifications to pick the course with the most credits if crdits are tied. Added tests to verify if it is working properly.

--- pegarCursoComMaisCreditosObrigatorios.js
+++ pegarCursoComMaisCreditosObrigatorios.js
@@ -1,25 +1,45 @@
 const calcularQtdCreditosObrigatorios = require("./calcularQtdCreditosObrigatorios");
 
 function pegarCursoComMaisCreditosObrigatorios(cursos) {
-  let cursoComMaisCreditosObrigatorios = {};
-  let creditosObrigatoriosDoCursoComMaisCreditos = 0;
+  let cursoMaisCreditosObrig = [];
+  let creditosObrigCursoMaisCreditos = 0;
 
   for (let i = 0; i < cursos.length; i++) {
-    let creditosObrigatoriosDoCursoAtual = calcularQtdCreditosObrigatorios(
-      cursos[i]
-    );
+    let creditosObrigAtual = calcularQtdCreditosObrigatorios(cursos[i]);
 
-    if (
-      creditosObrigatoriosDoCursoAtual >
-      creditosObrigatoriosDoCursoComMaisCreditos
-    ) {
-      cursoComMaisCreditosObrigatorios = cursos[i];
-      creditosObrigatoriosDoCursoComMaisCreditos =
-        creditosObrigatoriosDoCursoAtual;
+    //Créditos empatados
+    if (creditosObrigAtual === creditosObrigCursoMaisCreditos) {
+      //Mesmo tipo
+      if (cursos[i].tipo === cursoMaisCreditosObrig[0].tipo) {
+        cursoMaisCreditosObrig.push(cursos[i]);
+      }
+      //Tipo diferente
+      else if (
+        cursoMaisCreditosObrig[0].tipo === "Graduação" ||
+        (cursoMaisCreditosObrig[0].tipo === "Mestrado" &&
+          cursos[i].tipo === "Doutorado")
+      ) {
+        cursoMaisCreditosObrig = [cursos[i]];
+        creditosObrigCursoMaisCreditos = creditosObrigAtual;
+      }
+    }
+
+    //Créditos atuais maiores
+    if (creditosObrigAtual > creditosObrigCursoMaisCreditos) {
+      cursoMaisCreditosObrig = [cursos[i]];
+      creditosObrigCursoMaisCreditos = creditosObrigAtual;
     }
   }
 
-  return cursoComMaisCreditosObrigatorios;
+  return cursoMaisCreditosObrig.map((curso) => {
+    return {
+      Curso: curso.nome,
+      Disciplinas: curso.disciplinas.map((disciplina) => {
+        return { Descrição: disciplina.nome, Créditos: disciplina.creditos };
+      }),
+      ["Créditos Obrigatórios"]: creditosObrigCursoMaisCreditos,
+    };
+  });
 }
 
 module.exports = pegarCursoComMaisCreditosObrigatorios;
--- pegarCursoComMaisCreditosObrigatorios.test.js
+++ pegarCursoComMaisCreditosObrigatorios.test.js
@@ -31,15 +31,17 @@ test("Pegar curso com mais créditos", () => {
     },
   ];
 
-  expect(pegarCursoComMaisCreditosObrigatorios(cursos)).toEqual({
-    nome: "Curso02",
-    tipo: "Mestrado",
-    disciplinas: [
-      { nome: "Disciplina01", creditos: 6, obrigatoria: true },
-      { nome: "Disciplina02", creditos: 4, obrigatoria: true },
-      { nome: "Disciplina03", creditos: 5, obrigatoria: true },
-    ],
-  });
+  expect(pegarCursoComMaisCreditosObrigatorios(cursos)).toEqual([
+    {
+      Curso: "Curso02",
+      Disciplinas: [
+        { Descrição: "Disciplina01", Créditos: 6 },
+        { Descrição: "Disciplina02", Créditos: 4 },
+        { Descrição: "Disciplina03", Créditos: 5 },
+      ],
+      ["Créditos Obrigatórios"]: 15,
+    },
+  ]);
 });
 
 test("Pegar curso com mais créditos obrigatórios", () => {
@@ -73,20 +75,112 @@ test("Pegar curso com mais créditos obrigatórios", () => {
     },
   ];
 
-  expect(pegarCursoComMaisCreditosObrigatorios(cursos)).toEqual({
-    nome: "Curso03",
-    tipo: "Doutorado",
-    disciplinas: [
-      { nome: "Disciplina01", creditos: 4, obrigatoria: true },
-      { nome: "Disciplina02", creditos: 2, obrigatoria: false },
-      { nome: "Disciplina03", creditos: 3, obrigatoria: true },
-    ],
-  });
+  expect(pegarCursoComMaisCreditosObrigatorios(cursos)).toEqual([
+    {
+      Curso: "Curso03",
+      Disciplinas: [
+        { Descrição: "Disciplina01", Créditos: 4 },
+        { Descrição: "Disciplina02", Créditos: 2 },
+        { Descrição: "Disciplina03", Créditos: 3 },
+      ],
+      ["Créditos Obrigatórios"]: 7,
+    },
+  ]);
 });
 
-/*test("Pegar curso com mais créditos obrigatórios com desempate por tipo", () =>
-  expect(criarDisciplina("Algoritmos e Programação", 6, true)).toEqual({
-    nome: "Algoritmos e Programação",
-    creditos: 6,
-    obrigatoria: true,
-  }));*/
+test("Pegar curso com mais créditos obrigatórios: empatados por crédito e desempatados por tipo", () => {
+  const cursos = [
+    {
+      nome: "Curso01",
+      tipo: "Graduação",
+      disciplinas: [
+        { nome: "Disciplina01", creditos: 5, obrigatoria: false },
+        { nome: "Disciplina02", creditos: 3, obrigatoria: false },
+        { nome: "Disciplina03", creditos: 4, obrigatoria: true },
+      ],
+    },
+    {
+      nome: "Curso02",
+      tipo: "Mestrado",
+      disciplinas: [
+        { nome: "Disciplina01", creditos: 6, obrigatoria: false },
+        { nome: "Disciplina02", creditos: 4, obrigatoria: false },
+        { nome: "Disciplina03", creditos: 5, obrigatoria: true },
+      ],
+    },
+    {
+      nome: "Curso03",
+      tipo: "Doutorado",
+      disciplinas: [
+        { nome: "Disciplina01", creditos: 4, obrigatoria: false },
+        { nome: "Disciplina02", creditos: 2, obrigatoria: true },
+        { nome: "Disciplina03", creditos: 3, obrigatoria: true },
+      ],
+    },
+  ];
+
+  expect(pegarCursoComMaisCreditosObrigatorios(cursos)).toEqual([
+    {
+      Curso: "Curso03",
+      Disciplinas: [
+        { Descrição: "Disciplina01", Créditos: 4 },
+        { Descrição: "Disciplina02", Créditos: 2 },
+        { Descrição: "Disciplina03", Créditos: 3 },
+      ],
+      ["Créditos Obrigatórios"]: 5,
+    },
+  ]);
+});
+
+test("Pegar curso com mais créditos obrigatórios: empatados por crédito e tipo", () => {
+  const cursos = [
+    {
+      nome: "Curso01",
+      tipo: "Graduação",
+      disciplinas: [
+        { nome: "Disciplina01", creditos: 5, obrigatoria: false },
+        { nome: "Disciplina02", creditos: 3, obrigatoria: false },
+        { nome: "Disciplina03", creditos: 4, obrigatoria: true },
+      ],
+    },
+    {
+      nome: "Curso02",
+      tipo: "Doutorado",
+      disciplinas: [
+        { nome: "Disciplina01", creditos: 6, obrigatoria: false },
+        { nome: "Disciplina02", creditos: 4, obrigatoria: false },
+        { nome: "Disciplina03", creditos: 5, obrigatoria: true },
+      ],
+    },
+    {
+      nome: "Curso03",
+      tipo: "Doutorado",
+      disciplinas: [
+        { nome: "Disciplina01", creditos: 4, obrigatoria: false },
+        { nome: "Disciplina02", creditos: 2, obrigatoria: true },
+        { nome: "Disciplina03", creditos: 3, obrigatoria: true },
+      ],
+    },
+  ];
+
+  expect(pegarCursoComMaisCreditosObrigatorios(cursos)).toEqual([
+    {
+      Curso: "Curso02",
+      Disciplinas: [
+        { Descrição: "Disciplina01", Créditos: 6 },
+        { Descrição: "Disciplina02", Créditos: 4 },
+        { Descrição: "Disciplina03", Créditos: 5 },
+      ],
+      ["Créditos Obrigatórios"]: 5,
+    },
+    {
+      Curso: "Curso03",
+      Disciplinas: [
+        { Descrição: "Disciplina01", Créditos: 4 },
+        { Descrição: "Disciplina02", Créditos: 2 },
+        { Descrição: "Disciplina03", Créditos: 3 },
+      ],
+      ["Créditos Obrigatórios"]: 5,
+    },
+  ]);
+});
