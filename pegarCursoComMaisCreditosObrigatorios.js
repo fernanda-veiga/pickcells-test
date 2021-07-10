e7e80b3662c909ed2bdcb4847fc57eb67650bef5
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-07-10 16:16:36 -0300

    Changed the function calcularQtdCreditos to calcularQtdCreditosObrigatorios so that the main function returns the course with the most obligatory credits.

--- pegarCursoComMaisCreditosObrigatorios.js
+++ pegarCursoComMaisCreditosObrigatorios.js
@@ -1,11 +1,13 @@
-const calcularQtdCreditos = require("./calcularQtdCreditos");
+const calcularQtdCreditosObrigatorios = require("./calcularQtdCreditosObrigatorios");
 
 function pegarCursoComMaisCreditosObrigatorios(cursos) {
   let cursoComMaisCreditosObrigatorios = {};
   let creditosObrigatoriosDoCursoComMaisCreditos = 0;
 
   for (let i = 0; i < cursos.length; i++) {
-    let creditosObrigatoriosDoCursoAtual = calcularQtdCreditos(cursos[i]);
+    let creditosObrigatoriosDoCursoAtual = calcularQtdCreditosObrigatorios(
+      cursos[i]
+    );
 
     if (
       creditosObrigatoriosDoCursoAtual >
