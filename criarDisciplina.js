d3623e44c325ac53f5fd5b400eccc2c38f6efeb8
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-07-10 12:10:44 -0300

    Added a function that creates a course and returns an object with the course's name, credits and if it is obligatory or not. Added a test to evaluate if the course was being created correctly.

---
+++ criarDisciplina.js
@@ -0,0 +1,5 @@
+function criarDisciplina(nome, creditos, obrigatoria) {
+  return { nome, creditos, obrigatoria };
+}
+
+module.exports = criarDisciplina;
---
+++ criarDisciplina.test.js
@@ -0,0 +1,8 @@
+const criarDisciplina = require("./criarDisciplina");
+
+test("Criar uma discplina", () =>
+  expect(criarDisciplina("Algoritmos e Programação", 6, true)).toEqual({
+    nome: "Algoritmos e Programação",
+    creditos: 6,
+    obrigatoria: true,
+  }));
