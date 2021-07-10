6a6167a01ce240ed6a92487f26b9ed5b478efecb
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-07-10 12:18:38 -0300

    Added a mock disciplinas array and modified the test to fit the changes.

--- criarCurso.test.js
+++ criarCurso.test.js
@@ -1,29 +1,17 @@
 const criarCurso = require("./criarCurso");
+const disciplinas = [
+  { nome: "Algoritmos e Programação", creditos: 6, obrigatoria: true },
+  { nome: "Estatística", creditos: 2, obrigatoria: false },
+];
 
 test("Criar um curso", () =>
-  expect(
-    criarCurso(
-      "Ciência da Computação",
-      "Graduação",
-      [
-        "Algoritmos e Programação",
-        "Matemática Computacional",
-        "Tópicos Avançados em Bancos de Dados",
-        "Metodologia Científica",
+  expect(criarCurso("Ciência da Computação", "Graduação", disciplinas)).toEqual(
+    {
+      nome: "Ciência da Computação",
+      tipo: "Graduação",
+      disciplinas: [
+        { nome: "Algoritmos e Programação", creditos: 6, obrigatoria: true },
+        { nome: "Estatística", creditos: 2, obrigatoria: false },
       ],
-      ["Estatística", "Tópicos Avançados em Linguagens de Programação"]
-    )
-  ).toEqual({
-    nome: "Ciência da Computação",
-    tipo: "Graduação",
-    disciplinasObrigatórias: [
-      "Algoritmos e Programação",
-      "Matemática Computacional",
-      "Tópicos Avançados em Bancos de Dados",
-      "Metodologia Científica",
-    ],
-    disciplinasOpcionais: [
-      "Estatística",
-      "Tópicos Avançados em Linguagens de Programação",
-    ],
-  }));
+    }
+  ));
