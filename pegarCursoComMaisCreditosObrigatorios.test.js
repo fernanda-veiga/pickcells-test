6108f50a3ee3d3631b8ea2e094bd7012f4b55f13
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-07-10 16:02:26 -0300

    Added a test to the function that returns the course with most credits to see if it is working properly.

---
+++ pegarCursoComMaisCreditosObrigatorios.test.js
@@ -0,0 +1,57 @@
+const pegarCursoComMaisCreditosObrigatorios = require("./pegarCursoComMaisCreditosObrigatorios");
+
+test("Pegar curso com mais créditos", () => {
+  const cursos = [
+    {
+      nome: "Curso01",
+      tipo: "Graduação",
+      disciplinas: [
+        { nome: "Disciplina01", creditos: 5, obrigatoria: true },
+        { nome: "Disciplina02", creditos: 3, obrigatoria: true },
+        { nome: "Disciplina03", creditos: 4, obrigatoria: true },
+      ],
+    },
+    {
+      nome: "Curso02",
+      tipo: "Mestrado",
+      disciplinas: [
+        { nome: "Disciplina01", creditos: 6, obrigatoria: true },
+        { nome: "Disciplina02", creditos: 4, obrigatoria: true },
+        { nome: "Disciplina03", creditos: 5, obrigatoria: true },
+      ],
+    },
+    {
+      nome: "Curso03",
+      tipo: "Doutorado",
+      disciplinas: [
+        { nome: "Disciplina01", creditos: 4, obrigatoria: true },
+        { nome: "Disciplina02", creditos: 2, obrigatoria: true },
+        { nome: "Disciplina03", creditos: 3, obrigatoria: true },
+      ],
+    },
+  ];
+
+  expect(pegarCursoComMaisCreditosObrigatorios(cursos)).toEqual({
+    nome: "Curso02",
+    tipo: "Mestrado",
+    disciplinas: [
+      { nome: "Disciplina01", creditos: 6, obrigatoria: true },
+      { nome: "Disciplina02", creditos: 4, obrigatoria: true },
+      { nome: "Disciplina03", creditos: 5, obrigatoria: true },
+    ],
+  });
+});
+
+/*test("Pegar curso com mais créditos obrigatórios", () =>
+  expect(criarDisciplina("Algoritmos e Programação", 6, true)).toEqual({
+    nome: "Algoritmos e Programação",
+    creditos: 6,
+    obrigatoria: true,
+  }));*/
+
+/*test("Pegar curso com mais créditos obrigatórios com desempate por tipo", () =>
+  expect(criarDisciplina("Algoritmos e Programação", 6, true)).toEqual({
+    nome: "Algoritmos e Programação",
+    creditos: 6,
+    obrigatoria: true,
+  }));*/
