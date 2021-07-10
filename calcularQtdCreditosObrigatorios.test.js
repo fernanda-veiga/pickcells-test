2876b63c9da8397a8b35ff2c6cf5e6831cc13c06
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-07-10 16:14:04 -0300

    Added a test to evaluate if the function is calculating the obligatory credits properly.

---
+++ calcularQtdCreditosObrigatorios.test.js
@@ -0,0 +1,29 @@
+const calcularQtdCreditosObrigatorios = require("./calcularQtdCreditosObrigatorios");
+
+test("Calcular a quantidade de créditos de um curso", () => {
+  const curso = {
+    nome: "Curso01",
+    tipo: "Graduação",
+    disciplinas: [
+      { nome: "Disciplina01", creditos: 5, obrigatoria: true },
+      { nome: "Disciplina02", creditos: 3, obrigatoria: true },
+      { nome: "Disciplina03", creditos: 4, obrigatoria: true },
+    ],
+  };
+
+  expect(calcularQtdCreditosObrigatorios(curso)).toEqual(12);
+});
+
+test("Calcular a quantidade de créditos obrigatórios de um curso", () => {
+  const curso = {
+    nome: "Curso01",
+    tipo: "Graduação",
+    disciplinas: [
+      { nome: "Disciplina01", creditos: 5, obrigatoria: false },
+      { nome: "Disciplina02", creditos: 3, obrigatoria: true },
+      { nome: "Disciplina03", creditos: 4, obrigatoria: true },
+    ],
+  };
+
+  expect(calcularQtdCreditosObrigatorios(curso)).toEqual(7);
+});
